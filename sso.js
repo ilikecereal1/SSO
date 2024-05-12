889161413a33c060134dc24863f77c93b864f1e4
Isaac <windows11createrscratch@gmail.com>
2024-05-12 16:03:54 +0800

    b

--- sso.js
+++ sso.js
@@ -4,23 +4,23 @@ const userdata = [
     email: "admin@ravenfuture.com",
     userPassword:
       "240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9",
-  }
+  },
 ];
 
 function setCookie(cname, cvalue, exdays) {
   const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires="+ d.toUTCString();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function getCookie(cname) {
   let name = cname + "=";
   let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
     let c = ca[i];
-    while (c.charAt(0) == ' ') {
+    while (c.charAt(0) == " ") {
       c = c.substring(1);
     }
     if (c.indexOf(name) == 0) {
@@ -43,7 +43,7 @@ async function hashValue(message) {
 function check() {
   for (let i = 0; i < userdata.length; i++) {
     if (getCookie("token") == userdata[i]["email"]) {
-      document.getElementById("status").textContent = "Status: Logged in"
+      document.getElementById("status").textContent = "Status: Logged in";
     } else {
       window.location.href = `login.html?next=${window.location}`;
     }
@@ -53,30 +53,30 @@ function check() {
 function login() {
   for (let i = 0; i < userdata.length; i++) {
     if (getCookie("token") == userdata[i]["email"]) {
-      window.location = searchParams.get('next')
+      window.location = searchParams.get("next");
     }
   }
   let found = false;
   let email = prompt("Kindly enter your DJX or primary RavenFuture email.");
   for (let i = 0; i < userdata.length; i++) {
     if (email == userdata[i]["email"]) {
-        let password = prompt(
-            "Kindly enter your respective password for your account."
-          );
-          console.log(password);
-          hashValue(password).then((hash) => {
-            if (hash == userdata[i]["userPassword"]) {
-              found = true;
-              alert("Password is correct, kindly wait as we redirect you.");
-              window.location = searchParams.get('next')
-              setCookie("token", email, 30)
-            } else {
-              alert("Incorrect password, unable to redirect you, please retry.");
-            }
-          });
+      found = true;
+      let password = prompt(
+        "Kindly enter your respective password for your account."
+      );
+      console.log(password);
+      hashValue(password).then((hash) => {
+        if (hash == userdata[i]["userPassword"]) {
+          alert("Password is correct, kindly wait as we redirect you.");
+          window.location = searchParams.get("next");
+          setCookie("token", email, 30);
+        } else {
+          alert("Incorrect password, unable to redirect you, please retry.");
+        }
+      });
     }
   }
-  if (!(found)) {
-    alert("No account found.")
+  if (!found) {
+    alert("No account found.");
   }
 }
